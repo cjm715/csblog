00d2da7a14dfc45ae8850d7f2ce82c2a15dd542a
cjm715 <cjmiles2006@gmail.com>
2017-07-24 21:47:56 -0400

    added js

--- wics/predprey/animal.js
+++ wics/predprey/animal.js
@@ -1,25 +1,25 @@
-var s = new p5(function( p ) {
+var s = new p5(function(p) {
 
-  var x = 100;
-  var y = 100;
+    var x = 100;
+    var y = 100;
 
-  p.setup = function() {
-      var canvasWidth = 360;
-      var canvasHeight = 360;
+    p.setup = function() {
+        var canvasWidth = 360;
+        var canvasHeight = 360;
 
-      var canvasDiv = p.select('#canvasDiv-pp')
-      var sketchDiv = p.select('#sketch-holder-pp')
-      //sketchDiv.style('position: relative')
-      canvas = p.createCanvas(canvasWidth, canvasHeight);
-      //canvas = createCanvas(360, 360);
-      canvas.parent(canvasDiv);
+        var canvasDiv = p.select('#canvasDiv-pp')
+        var sketchDiv = p.select('#sketch-holder-pp')
+        //sketchDiv.style('position: relative')
+        canvas = p.createCanvas(canvasWidth, canvasHeight);
+        //canvas = createCanvas(360, 360);
+        canvas.parent(canvasDiv);
 
-    
-  };
 
-  p.draw = function() {
-    p.background(0);
-    p.fill(255);
-    p.rect(x,y,50,50);
-  };
+    };
+
+    p.draw = function() {
+        p.background(0);
+        p.fill(255);
+        p.rect(x, y, 50, 50);
+    };
 });
